bc182c9d13df84992d97d4e94cca64114ca916aa
Alena <kisamkis@yandex.ru>
2022-01-17 22:07:03 +0300

    templated cards with menus using classes

--- js/script.js
+++ js/script.js
@@ -132,5 +132,42 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     window.addEventListener('scroll', showModalByScroll);
 
-    
+    // Шаблонизация создания карточек с меню на странице 
+
+    const contain = document.querySelector('.menu__field .container');
+    class CreateBlock {
+        constructor(jpg, alt, name, descr, price, parentSelector){
+            this.jpg = jpg;
+            this.alt = alt;
+            this.name = name;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+        }
+
+        addMenu(){   
+            let block = document.createElement(`div`);
+            block.innerHTML = `
+                <div class="menu__item">
+                    <img src="${this.jpg}" alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">Меню "${this.name}"</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>`;
+            this.parent.append(block);
+        }
+    }
+    new CreateBlock('img/tabs/vegy.jpg', 'vegy', 'Фитнес',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+     229, '.menu__field .container').addMenu();
+    new CreateBlock('img/tabs/elite.jpg', 'elite', 'Премиум',
+     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+      550,'.menu__field .container').addMenu();
+    new CreateBlock('img/tabs/post.jpg', 'post', 'Постное',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    430,'.menu__field .container').addMenu();
 });
