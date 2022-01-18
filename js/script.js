5b33726b5d0e7af4c33542648dba4a9c43460958
Alena <kisamkis@yandex.ru>
2022-01-18 23:04:24 +0300

    using rest operator

--- js/script.js
+++ js/script.js
@@ -136,27 +136,33 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const contain = document.querySelector('.menu__field .container');
     class CreateBlock {
-        constructor(jpg, alt, name, descr, price, parentSelector){
+        constructor(jpg, alt, name, descr, price, parentSelector, ...classes){
             this.jpg = jpg;
             this.alt = alt;
             this.name = name;
+            this.classes = classes;
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parentSelector);
         }
 
         addMenu(){   
-            let block = document.createElement(`div`);
+            const block = document.createElement(`div`);
+            if(this.classes.length === 0){
+                this.classes = block.classList.add('menu__item');
+            }else{
+                this.classes.forEach(className => block.classList.add(className));            
+            }
+
+
             block.innerHTML = `
-                <div class="menu__item">
-                    <img src="${this.jpg}" alt="${this.alt}">
-                    <h3 class="menu__item-subtitle">Меню "${this.name}"</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src="${this.jpg}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">Меню "${this.name}"</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>`;
             this.parent.append(block);
         }
