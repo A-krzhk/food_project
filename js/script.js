93ff68c3c0ec37b8e591e233a2fa29cdbb514f01
Alena <kisamkis@yandex.ru>
2022-01-28 14:44:03 +0300

    sending data to the server from forms on the page

--- js/script.js
+++ js/script.js
@@ -176,4 +176,57 @@ window.addEventListener('DOMContentLoaded', () => {
     new CreateBlock('img/tabs/post.jpg', 'post', 'Постное',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     430,'.menu__field .container').addMenu();
+
+    // forms
+
+    const forms = document.querySelectorAll('form');
+    const mess = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const statusMessage = document.createElement('div');
+            statusMessage.textContent = mess.loading;
+            form.append(statusMessage);
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData .forEach((value, key) => {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+    // Отслеживаем load (конечную загрузку нашего запроса)
+            request.addEventListener('load', () => {
+                if(request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = mess.success;
+
+                    // очистить форму 
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = mess.failure;
+                }
+            }); 
+        });
+    } 
+
+
 });
